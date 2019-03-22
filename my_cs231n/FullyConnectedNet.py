9d0e99bf43748994ffa195d4714de9c2ff6eee71
SOUSIC <517048082@qq.com>
2019-03-22 17:27:27 +0800

    Add files via upload

---
+++ my_cs231n/FullyConnectedNet.py
@@ -0,0 +1,8 @@
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+from fc_net import *
+from gradient_check import eval_numerical_gradient, eval_numerical_gradient_array
+from solver import Solver
+
+
---
+++ my_cs231n/kNN.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+class KNearestNeighbor(object):
+  """ a kNN classifier with L2 distance """
+
+  def __init__(self):
+    pass
+
+  def train(self, X, y):
+    """
+    Train the classifier. For k-nearest neighbors this is just 
+    memorizing the training data.
+
+    Inputs:
+    - X: A numpy array of shape (num_train, D) containing the training data
+      consisting of num_train samples each of dimension D.
+    - y: A numpy array of shape (N,) containing the training labels, where
+         y[i] is the label for X[i].
+    """
+    self.X_train = X  #X的维度是：（样本数，样本维度）
+    self.y_train = y  #y的维度是：（标签数，）y[i]代表第i个样本的标签
+    
+  def predict(self, X, k=1):
+    """
+    Predict labels for test data using this classifier.
+
+    Inputs:
+    - X: A numpy array of shape (num_test, D) containing test data consisting
+         of num_test samples each of dimension D.
+         X：（需要预测例子的数量，每个例子的维度）
+    - k: The number of nearest neighbors that vote for the predicted labels.
+        k=1时就是NN
+
+
+    Returns:
+    - y: A numpy array of shape (num_test,) containing predicted labels for the
+      test data, where y[i] is the predicted label for the test point X[i].  
+    """
+    dists = self.compute_distances(X)
+    return self.predict_labels(dists, k=k)
+
+  def compute_distances(self, X):
+    """
+    Compute the distance between each test point in X and each training point
+    in self.X_train using no explicit loops.
+ 
+    Input / Output: Same as compute_distances_two_loops
+    """
+    num_test = X.shape[0]
+    num_train = self.X_train.shape[0]
+    dists = np.zeros((num_test, num_train)) 
+    #(a-b)^2=a^2+b^2-2ab
+    dists = np.sqrt(-2*np.dot(X, self.X_train.T) + np.sum(np.square(self.X_train), axis = 1) + np.transpose([np.sum(np.square(X), axis = 1)]))
+    #dists的维度是（测试数，训练数）
+    return dists
+
+  def predict_labels(self, dists, k=1):
+    """
+    Given a matrix of distances between test points and training points,
+    predict a label for each test point.
+
+    Inputs:
+    - dists: A numpy array of shape (num_test, num_train) where dists[i, j]
+      gives the distance betwen the ith test point and the jth training point.
+
+    Returns:
+    - y: A numpy array of shape (num_test,) containing predicted labels for the
+      test data, where y[i] is the predicted label for the test point X[i].  
+    """
+    num_test = dists.shape[0]
+    y_pred = np.zeros(num_test)
+    for i in xrange(num_test):
+      # A list of length k storing the labels of the k nearest neighbors to
+      # the ith test point.
+      closest_y = []
+      #########################################################################
+      # TODO:                                                                 #
+      # Use the distance matrix to find the k nearest neighbors of the ith    #
+      # testing point, and use self.y_train to find the labels of these       #
+      # neighbors. Store these labels in closest_y.                           #
+      # Hint: Look up the function numpy.argsort.                             #
+      #########################################################################   
+      closest_y = self.y_train[np.argsort(dists[i])[:k]]#取y_train的对应标签值
+      #########################################################################
+      # TODO:                                                                 #
+      # Now that you have found the labels of the k nearest neighbors, you    #
+      # need to find the most common label in the list closest_y of labels.   #
+      # Store this label in y_pred[i]. Break ties by choosing the smaller     #
+      # label.                                                                #
+      #########################################################################
+      y_pred[i] = np.argmax(np.bincount(closest_y))
+    return y_pred
+
